ec5fe7ab444cf96d9dfbdf8558ad1990d375bba8
Sven <SvenRocho@gmx.de>
2014-11-25 12:12:45 +0100

    Benutzerberechtigung mit htaccess hinzugefügt
    Pfad zur .htpasswd muss in der .htaccess angepasst werden. Wichtig, der
    absolute Pfad muss angegeben werden

--- prototyp2/upload.php
+++ prototyp2/upload.php
@@ -1,7 +1,16 @@
 <h1>Willkommen beim Bild-Upload-Skript</h1>
 <form action="check.php" method="post" enctype="multipart/form-data">
-Bitte Namen eingeben: <input type="text" name="username"><br>
-Bild auswaehlen: <input type="file" name="datei"><br>
+	<table>
+		<tr>
+			<td>Bitte Namen eingeben:</td>
+			<td><input type="text" name="username"></td>
+		</tr>
+		<tr>
+			<td>Bild auswaehlen:</td>
+			<td><input type="file" name="datei"></td>
+		</tr>
+	</table>
+ <br>
 <input type="submit" value="Hochladen">
 </form>
 Nur Bilder bis 100 KB im jpg oder gif-Format!
