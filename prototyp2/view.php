200fc9758ef8d2ae87430e224d869bb22507c893
s137129 <s137129@WS04A127.infolabs.test>
2014-11-25 12:02:11 +0100

    view hinzugefügt

---
+++ prototyp2/view.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<div align="left" width="10%">
+Nächste News
+<div>A</div>
+<div>B</div>
+<div>C</div>
+<div>D</div>
+<div>E</div>
+<div>F</div>
+<div>G</div>
+<div>H</div>
+<div>I</div>
+</div>
+<div align="right"></div>
