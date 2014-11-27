cb78f9c687ea5b5768617e493b352dd76f3ec1fb
s137129 <s137129@hft-leipzig.de>
2014-11-27 19:49:47 +0100

    config.php hinzugefügt
    config.php um alle Änderungen zentral vornehmen zu können.

---
+++ prototyp2/config.php
@@ -0,0 +1,16 @@
+/*
+
+Dies ist die Konfigurationsdatei.
+Hier können alle nötigen Einstellungen vorgenommen werden.
+
+Bitte achten sie auf die Kommentare hinter den Einstellungen.
+
+*/
+<?php
+
+$max=10; // maximale Anzahl an News min. 2!!!
+$slidetime = 5; // Bitte ca. 1-2 Sekunde mehr geben, da das laden der seite mit berücksichtigt werden muss
+                // Des weiteren muss die Zahl ganzzahlig sein.
+$ordner = "./upload/";  // hier muss der korrekte weg zum Bilderordner angegeben werden.
+                        // Bitte achten sie darauf, dass sie den Ordner für die freigegeben Bilder auswählen.
+?>
