e7bbb74a14bb47e0cd36dc97ff57287e0529bdeb
s137129 <s137129@hft-leipzig.de>
2014-12-03 22:07:46 +0100

    view angepasst + Bilder ausgetauscht
    hoffe es gefällt soweit :)
    HD Bilder hinzugefügt

--- prototyp2/view.php
+++ prototyp2/view.php
@@ -35,56 +35,26 @@
 <meta http-equiv="refresh" content="<?php echo $slidetime; ?>;URL=<?php echo './view.php?zeile=',$zeile; ?>">
 </head>
 <body height="100%">
-
-<table border="1" height="100%" width="100%">
+<table border="0" height="100%" width="100%">
         <tr>
                 <td>
-                        <center>
-                                Nächste News
-                        </center>
-                </td>
-                <td>
-                        <center>
-                                Newsfenster
-                        </center>
-                </td>
-        </tr>
-        <tr  max-height="10%">
-                <td height="10%">
+<center>
                         <?php
                         if($zeile>$max) $zeile=1;
                         $hilfzeile=$zeile;
                         $hilfzeile++;
                         if($hilfzeile>$files_count) $hilfzeile=1;
-                        ?><center><img alt="<?php echo $arr;?>" src="<?php echo $arr[$hilfzeile]; ?>" width="100" align="middle"></center>
-                </td>
-                <td width="90%" rowspan="9" height="90%">
-                        <center><img alt="<?php echo $arr;?>" src="<?php echo $arr[$zeile]; ?>" width="90%" align="middle"></center>
-                </td>
-        </tr>
-        <?php
-                for($i=1; $i<$files_count-1; $i++) {?>
-                        <tr height="10%">
-                <td >
-                        <?php
-                                //Possition für nächste Bilder
+                        for($i=1; $i<$files_count-1; $i++) {
                         $hilfzeile++;
                         if($hilfzeile>$files_count) $hilfzeile=1;
-                        ?><center><img alt="<?php echo $arr;?>" src="<?php echo $arr[$hilfzeile]; ?>" width="100" align="middle"></center>
+
+                        ?><img alt="<?php echo $arr;?>" src="<?php echo $arr[$hilfzeile]; ?>" height="100" align="middle">
+                <?php }?>
+                </center></td>
+                <td width="90%" height="90%">
+                        <center><img alt="<?php echo $arr;?>" src="<?php echo $arr[$zeile]; ?>" width="80%"  align="middle"></center>
                 </td>
         </tr>
-                <?php }
 
-                        for($i++;$i<$max;$i++){
-                ?>
-                        <tr max-height="10%">
-                                <td ><?php
-                                                echo "keine Weiteren Informationen vorhanden!";
-                                                //Platzhalter
-                                        ?></td>
-                        </tr>
-                <?php
-                        }
-                ?>
 </table>
 </body>
