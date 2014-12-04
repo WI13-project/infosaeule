fec7316cb5589b0572b6be10972789d75b60841f
s137129 <s137129@hft-leipzig.de>
2014-12-04 14:54:08 +0100

    view update auf 16:9 fullview only

--- prototyp2/view.php
+++ prototyp2/view.php
@@ -10,7 +10,7 @@
                 //echo $zeile;
                 $arr[]=array();
                 //Bilder mit getimagesize (siehe check.php) pruefen!
-                $files = scandir($ordner);  //Pfad zum View-Ordner ggf. bei Ã¤nderungen anpassen!
+                $files = scandir($ordner);  //Pfad zum View-Ordner ggf. bei änderungen anpassen!
                 $files_count = count($files)-2; // Minus zwei wegen "." und ".."
                 // echo $files_count;
                 if($files_count>$max)  $files_count=$max;                                // setzt filescount auf Anzahl max herab, wenn es mehr Bilder sind als erlaubt;
@@ -34,27 +34,24 @@
 <head>
 <meta http-equiv="refresh" content="<?php echo $slidetime; ?>;URL=<?php echo './view.php?zeile=',$zeile; ?>">
 </head>
-<body height="100%">
-<table border="0" height="100%" width="100%">
+<body style="height:100%; vertical-align:middle; background-color:black; overflow:hidden">
+<center><table border="0" height="100%" width="100%">
         <tr>
-                <td>
+                <td width="10%">
 <center>
                         <?php
                         if($zeile>$max) $zeile=1;
                         $hilfzeile=$zeile;
-                        $hilfzeile++;
-                        if($hilfzeile>$files_count) $hilfzeile=1;
-                        for($i=1; $i<$files_count-1; $i++) {
+                        for($i=1; $i<$files_count; $i++) {
                         $hilfzeile++;
                         if($hilfzeile>$files_count) $hilfzeile=1;
 
-                        ?><img alt="<?php echo $arr;?>" src="<?php echo $arr[$hilfzeile]; ?>" height="100" align="middle">
-                <?php }?>
+                        ?><img alt="<?php echo $arr;?>" src="<?php echo $arr[$hilfzeile]; ?>" width="100%" align="middle"><br>
+                <?php } ?>
                 </center></td>
                 <td width="90%" height="90%">
-                        <center><img alt="<?php echo $arr;?>" src="<?php echo $arr[$zeile]; ?>" width="80%"  align="middle"></center>
+                        <center><img alt="<?php echo $arr;?>" src="<?php echo $arr[$zeile]; ?>" width="99%"  align="middle"></center>
                 </td>
         </tr>
-
-</table>
+</table></center>
 </body>
