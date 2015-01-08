93cc54412f8665e33569cac033c27186d8d0e052
Sven <SvenRocho@gmx.de>
2015-01-08 23:01:58 +0100

    Bootstrap.js wieder nach unten gepackt
    mal schauen ob das der fehler ist, bei mir funktionierts auch so..

--- prototyp2/header.php
+++ prototyp2/header.php
@@ -2,10 +2,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
  <head>
-  <title>Infos&auml;ule</title> <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+  <title>Infos&auml;ule</title>
   	<script language="JavaScript" type="text/javascript" src="js/header.js" ></script>
   	<link href="css/bootstrap.min.css" rel="stylesheet">
   	<link href="css/nav.css" rel="stylesheet">
@@ -49,6 +46,9 @@
 			</div><!--/.container-fluid -->
 		</nav>  	
 	</div>
-	
+	 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
